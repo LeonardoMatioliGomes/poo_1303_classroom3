8c6b3f966a414b96d6d10ba2291efa9e9de622f0
LeonardoMatioliGomes <127799008+LeonardoMatioliGomes@users.noreply.github.com>
2023-04-03 21:31:37 -0300

    Update bhaskara.cpp

--- bhaskara.cpp
+++ bhaskara.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int main() {
+    //interface para usuario inserir os valores
     float a, b, c, x1, x2, delta;
     cout <<"digite o valor de a:";
     cin >> a;
@@ -11,17 +12,17 @@ int main() {
     cin >> b;
     cout <<"digite o valor de c:";
     cin >> c;
-    
+    //formula para fazer bhaskara
     delta = pow(b, 2) -4 * a * c;
     
     if(delta = a ) {
         x1 = (b + sqrt(delta))/(2*a);
         x2 = (b + sqrt(delta))/(2*a);
-    
+    //resposta da formula
         cout << "as raizes da equação" <<x1<< "e" <<x2<< endl;
     }
 else  {
-    //
+    //caso não tenha resposta
     cout <<"a equacao nao tem raises reais." << endl;
 }
 
